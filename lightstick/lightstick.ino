a71007b8802c8a4bcff6a27aecd65fc23c4a4049
Anthony Chiam <git@atpc.xyz>
2021-12-02 01:11:47 +1000

    Update program according to client's requirements
    Functionality modified to operate as a red stick, single led travels slowly away from the base. One button changes the *color between green and red*, the other changes the *direction*.

--- lightstick/lightstick.ino
+++ lightstick/lightstick.ino
@@ -1,5 +1,5 @@
 /*
- * LightStick v1
+ * LightStick v1.1
  * 
  */
 #include <Adafruit_NeoPixel.h>
@@ -11,12 +11,13 @@
 // Button Debounce time.
 unsigned long debounceDelay = 50;
 
+unsigned long programDelay = 1000;
 
 /* Pin Definitions. --------------------------------*/
 // Strip.
 #define PIN_STRIP 9
 // Buttons. [YELLOW=D6, GREEN=D7]
-#define PIN_PAUSE 7
+#define PIN_COLOUR 7
 #define PIN_MODE 6
 // LEDs.
 #define PIN_LED1 5
@@ -26,24 +27,28 @@ unsigned long debounceDelay = 50;
 
 /* Constants. --------------------------------------*/
 // Program Modes.
-#define MODE_TOTAL 4
+#define MODE_TOTAL 2
 #define MODE_1 0
 #define MODE_2 1
 #define MODE_3 2
 #define MODE_4 3
 
+// Program Colours.
+#define COLOUR_TOTAL 2
+#define COLOUR_RED 0
+#define COLOUR_GREEN 1
 
 /* Variables. --------------------------------------*/
 // WS2812 Pixels.
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_STRIP, PIN_STRIP, NEO_GRB + NEO_KHZ800);
 
 int mode = 0;                 // Program Mode.
-bool pause = false;               // Program Colour.
+int colour = 0;               // Program Colour.
 
 // Button variables.
-int bsPause = HIGH;          // current reading from the PAUSE button.
-int lbsPause = HIGH;         // previous reading from the PAUSE button.
-unsigned long lbtPause = 0;  // Last time the state was toggled.
+int bsColour = HIGH;          // current reading from the colour button.
+int lbsColour = HIGH;         // previous reading from the colour button.
+unsigned long lbtColour = 0;  // Last time the state was toggled.
 int bsMode = HIGH;
 int lbsMode = HIGH;
 unsigned long lbtMode = 0;
@@ -57,7 +62,7 @@ void setup() {
 
   // Set pin modes.
   pinMode(PIN_STRIP, OUTPUT);
-  pinMode(PIN_PAUSE, INPUT_PULLUP);
+  pinMode(PIN_COLOUR, INPUT_PULLUP);
   pinMode(PIN_MODE, INPUT_PULLUP);
   pinMode(PIN_LED1, OUTPUT);
   pinMode(PIN_LED2, OUTPUT);
@@ -66,8 +71,8 @@ void setup() {
 
   // Initial mode state.
   mode = MODE_1;
-  pause = false;
-  setLED(mode, pause);
+  colour = COLOUR_RED;
+  setLED(mode);
 
   strip.begin();
   strip.setBrightness(50);
@@ -78,21 +83,21 @@ void setup() {
 void loop() {
 
   button_read();  // Process button actions.
-  setLED(mode, pause);   // Change mode indicator.
+  setLED(mode);   // Change mode indicator.
 
   // Program Switch.
   switch(mode) {
     case MODE_4:
-      handleM4();
+    
       break;
     case MODE_3:
-      handleM3();
+
       break;
     case MODE_2:
-      handleM2();
+      handleReverseSequential();
       break;
     case MODE_1:
-      handleM1();
+      handleSequential();
       break;
     default:
       // Fault. Revert to MODE_1.
@@ -103,70 +108,31 @@ void loop() {
 }
 
 // Set the specific LED based on the mode.
-void setLED(int m, bool p) {
+void setLED(int m) {
+
+  // Switch all LEDs off.
+  digitalWrite(PIN_LED1, LOW);
+  digitalWrite(PIN_LED2, LOW);
+  digitalWrite(PIN_LED3, LOW);
+  digitalWrite(PIN_LED4, LOW);
 
   switch(m){
     case MODE_4:
-      if(p){ 
-        digitalWrite(PIN_LED1, HIGH);
-        digitalWrite(PIN_LED2, HIGH);
-        digitalWrite(PIN_LED3, HIGH);
-        digitalWrite(PIN_LED4, LOW); 
-      } else { 
-        digitalWrite(PIN_LED1, LOW);
-        digitalWrite(PIN_LED2, LOW);
-        digitalWrite(PIN_LED3, LOW);
-        digitalWrite(PIN_LED4, HIGH); 
-      }
-
+      digitalWrite(PIN_LED4, HIGH);
       break;
     case MODE_3:
-      if(p){ 
-        digitalWrite(PIN_LED1, HIGH);
-        digitalWrite(PIN_LED2, HIGH);
-        digitalWrite(PIN_LED3, LOW);
-        digitalWrite(PIN_LED4, HIGH); 
-      } else { 
-        digitalWrite(PIN_LED1, LOW);
-        digitalWrite(PIN_LED2, LOW);
-        digitalWrite(PIN_LED3, HIGH);
-        digitalWrite(PIN_LED4, LOW); 
-      }
-
+      digitalWrite(PIN_LED3, HIGH);
       break;
     case MODE_2:
-      if(p){ 
-        digitalWrite(PIN_LED1, HIGH);
-        digitalWrite(PIN_LED2, LOW);
-        digitalWrite(PIN_LED3, HIGH);
-        digitalWrite(PIN_LED4, HIGH); 
-      } else { 
-        digitalWrite(PIN_LED1, LOW);
-        digitalWrite(PIN_LED2, HIGH);
-        digitalWrite(PIN_LED3, LOW);
-        digitalWrite(PIN_LED4, LOW); 
-      }
-
+      digitalWrite(PIN_LED2, HIGH);
       break;
     case MODE_1:
-      if(p){ 
-        digitalWrite(PIN_LED1, LOW);
-        digitalWrite(PIN_LED2, HIGH);
-        digitalWrite(PIN_LED3, HIGH);
-        digitalWrite(PIN_LED4, HIGH); 
-      } else { 
-        digitalWrite(PIN_LED1, HIGH);
-        digitalWrite(PIN_LED2, LOW);
-        digitalWrite(PIN_LED3, LOW);
-        digitalWrite(PIN_LED4, LOW); 
-      }
-
+      digitalWrite(PIN_LED1, HIGH);
       break;
     default:
       // Keep all LEDs off.
       break;
   }
-  
 }
 
 // Set a single RGB at i, coloured c.
@@ -188,31 +154,32 @@ void setSingleStrip(uint16_t i, uint32_t c) {
 }
 
 // Process actions from the buttons.
-// Alters 'pause' and 'mode' variables.
+// Alters 'colour' and 'mode' variables.
 void button_read() {
 
-  // Read PIN_PAUSE.
-  int readingPause = digitalRead(PIN_PAUSE);
+  // Read PIN_COLOUR.
+  int readingColour = digitalRead(PIN_COLOUR);
 
   // Reset the debouncing timer if changed due to noise.
-  if(readingPause != lbsPause) {
-    lbtPause = millis();
+  if(readingColour != lbsColour) {
+    lbtColour = millis();
   }
 
-  if((millis() - lbtPause) > debounceDelay) {
+  if((millis() - lbtColour) > debounceDelay) {
     // if button state has changed.
-    if(readingPause != bsPause) {
-      bsPause = readingPause;
+    if(readingColour != bsColour) {
+      bsColour = readingColour;
 
       // Only toggle state if new button state is high.
-      if(bsPause == HIGH) {
+      if(bsColour == HIGH) {
         // If button is successfully registered,
-        pause = !pause;
+        // Switch colours.
+        colour = (colour + 1) % COLOUR_TOTAL; // Change colour.
       }
     }
   }
   // Save the button reading.
-  lbsPause = readingPause;
+  lbsColour = readingColour;
 
 
   // Read PIN_MODE.
@@ -241,78 +208,71 @@ void button_read() {
 }
 
 
-/*
- * MODE_1 - RED Sequential.
- */
-void handleM1() {
+
+
+//// Handle the off mode.
+//void handleOff() {
+//  for(int x = 0; x < NUM_STRIP; x++) {
+//    strip.setPixelColor(x, 0);
+//  }
+//  strip.show();
+//}
+
+void handleSequential() {
 
   if((millis() - lastTimeProgram) > 1000) {
     lastTimeProgram = millis();
 
-    // Move to next LED.
-    if(!pause) {
-      currentStrip = (currentStrip + 1) % NUM_STRIP;
+    // Switch colour.
+    uint32_t c;
+    switch(colour) {
+      case COLOUR_GREEN:
+        c = strip.Color(0,255,0);
+        break;
+      case COLOUR_RED:
+        c = strip.Color(255,0,0);
+        break;
+      default:
+        colour = COLOUR_RED;
+        c = strip.Color(255,0,0);
     }
 
     // Turn the LED on.
-    setSingleStrip(currentStrip, strip.Color(255,0,0));
+    setSingleStrip(currentStrip, c);
+
+    // Move to next LED.
+    currentStrip = (currentStrip + 1) % NUM_STRIP;
+
 
   } /* END program delay. */
 }
 
-/*
- * MODE_2 - GREEN Sequential.
- */
-void handleM2() {
+
+void handleReverseSequential() {
 
   if((millis() - lastTimeProgram) > 1000) {
     lastTimeProgram = millis();
 
-    // Move to next LED.
-    if(!pause) {
-      currentStrip = (currentStrip + 1) % NUM_STRIP;
+    // Switch colour.
+    uint32_t c;
+    switch(colour) {
+      case COLOUR_GREEN:
+        c = strip.Color(0,255,0);
+        break;
+      case COLOUR_RED:
+        c = strip.Color(255,0,0);
+        break;
+      default:
+        colour = COLOUR_RED;
+        c = strip.Color(255,0,0);
     }
 
     // Turn the LED on.
-    setSingleStrip(currentStrip, strip.Color(0,255,0));
-
-  } /* END program delay. */
-}
-
-/*
- * MODE_3 - RED Reverse Sequential.
- */
-void handleM3() {
-
-  if((millis() - lastTimeProgram) > 1000) {
-    lastTimeProgram = millis();
+    setSingleStrip(currentStrip, c);
 
     // Move to next LED.
-    if(!pause) {
-      currentStrip = (NUM_STRIP + currentStrip - 1) % NUM_STRIP;
-    }
+    currentStrip = (NUM_STRIP + currentStrip - 1) % NUM_STRIP;
 
-    // Turn the LED on.
-    setSingleStrip(currentStrip, strip.Color(255,0,0));
-
-  } /* END program delay. */
-}
-
-/*
- * MODE_4 - GREEN Reverse Sequential.
- */
-void handleM4() {
-
-  if((millis() - lastTimeProgram) > 1000) {
-    lastTimeProgram = millis();
-
-    // Move to next LED.
-    if(!pause) {
-      currentStrip = (NUM_STRIP + currentStrip - 1) % NUM_STRIP;
-    }
-
-    // Turn the LED on.
-    setSingleStrip(currentStrip, strip.Color(0,255,0));
 
   } /* END program delay. */
 }
